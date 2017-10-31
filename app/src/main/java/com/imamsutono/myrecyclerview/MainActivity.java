39952b31dafed151eec1ae5e95e58c42872f448f
imamsutono <imamssutono@gmail.com>
2017-10-31 09:41:29 +0700

    add recyclerview options menu

--- app/src/main/java/com/imamsutono/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/imamsutono/myrecyclerview/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -30,5 +32,30 @@ public class MainActivity extends AppCompatActivity {
         ListPresidentAdapter listPresidentAdapter = new ListPresidentAdapter(this);
         listPresidentAdapter.setListPresident(list);
         rvCategory.setAdapter(listPresidentAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_list:
+
+                break;
+
+            case R.id.action_grid:
+
+                break;
+
+            case R.id.action_cardview:
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
