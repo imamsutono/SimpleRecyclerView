da513abdd129e03f1aa0e200f9058e0f7ba161ec
imamsutono <imamssutono@gmail.com>
2017-10-31 08:14:16 +0700

    initial commit

---
+++ app/src/main/java/com/imamsutono/myrecyclerview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.imamsutono.myrecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvCategory;
+    private ArrayList<President>list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvCategory = (RecyclerView)findViewById(R.id.rv_category);
+        rvCategory.setHasFixedSize(true);
+
+        list = new ArrayList<>();
+        list.addAll(PresidentData.getListData());
+
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        ListPresidentAdapter listPresidentAdapter = new ListPresidentAdapter(this);
+        listPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(listPresidentAdapter);
+    }
+}
---
+++ app/src/main/java/com/imamsutono/myrecyclerview/President.java
@@ -0,0 +1,33 @@
+package com.imamsutono.myrecyclerview;
+
+/**
+ * Created by idekreasi on 10/30/17.
+ */
+
+public class President {
+    private String name, remarks, photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
